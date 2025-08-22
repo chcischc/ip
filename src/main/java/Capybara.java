7e61a947f5f590dc239ec0f3a9e32f9f6135e81d
Zhang Haoxiang <zkyiszky@gmail.com>
2025-08-22 10:16:00 +0800

    Implement level-0, greet and exit

--- src/main/java/Capybara.java
+++ src/main/java/Capybara.java
@@ -1,10 +1,15 @@
-public class Duke {
+public class Capybara {
+    private static final String LINE =
+            "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println(LINE);
+        System.out.println(" Hello! I'm Capybara");
+        System.out.println(" What can I do for you?");
+        System.out.println(LINE);
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
