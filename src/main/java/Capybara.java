a189a8ba0cda615c212b4bbe5005bffef1290fbf
Zhang Haoxiang <zkyiszky@gmail.com>
2025-08-22 11:04:51 +0800

    Implement level-1

--- src/main/java/Capybara.java
+++ src/main/java/Capybara.java
@@ -1,15 +1,41 @@
+import java.util.Scanner;
+
 public class Capybara {
     private static final String LINE =
             "____________________________________________________________";
 
+    private static void printMessage(String message) {
+        System.out.println(LINE);
+        System.out.println(message);
+        System.out.println(LINE);
+    }
+
+    private static final String LOGO =
+            "     _🍊_          \n"
+            + "   (´ ᴖ `)       \n"
+            + "  (-┬- -┬-)       \n"
+            + "  (_______)       \n";
+
     public static void main(String[] args) {
 
         System.out.println(LINE);
+        System.out.println(LOGO);
         System.out.println(" Hello! I'm Capybara");
-        System.out.println(" What can I do for you?");
+        System.out.println(" What can I do for ... Zzzzz");
         System.out.println(LINE);
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+            printMessage(input);
+        }
+        sc.close();
+
     }
 }
