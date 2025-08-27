8e4420746cce791f1b7a36446248be35b0323fc3
Zhang Haoxiang <zkyiszky@gmail.com>
2025-08-28 00:01:24 +0800

    Add method description for load() and save() in Storage class

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -8,6 +8,16 @@ public class Storage {
         this.filePath = filePath;
     }
 
+    /**
+     * Saves the current task list to the storage file.
+     * <p>
+     * Each task will be written in a text-based format defined by its
+     * {@code toFileString()} representation. If the parent directory does not
+     * exist, it will be created automatically.
+     *
+     * @param tasks the list of tasks to save to disk
+     * @throws IOException if an I/O error occurs while writing to the file
+     */
     public void save(ArrayList<Task> tasks) throws IOException {
         File file = new File(filePath);
         file.getParentFile().mkdirs();
@@ -18,6 +28,16 @@ public class Storage {
         writer.close();
     }
 
+    /**
+     * Loads the task list from the storage file.
+     * <p>
+     * If the file or parent directory does not exist, they will be created and
+     * an empty task list will be returned. Lines in the file that do not follow
+     * the expected format will be skipped.
+     *
+     * @return an {@code ArrayList<Task>} containing the tasks read from file
+     * @throws IOException if an I/O error occurs while creating or reading the file
+     */
     public ArrayList<Task> load() throws IOException {
         ArrayList<Task> tasks = new ArrayList<>();
         File file = new File(filePath);
