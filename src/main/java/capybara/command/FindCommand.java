156ed0967a9e316340b704132c2da9053be0daca
Zhang Haoxiang <zkyiszky@gmail.com>
2025-09-03 10:00:09 +0800

    Docs: add Javadoc for FindCommand

--- src/main/java/capybara/command/FindCommand.java
+++ src/main/java/capybara/command/FindCommand.java
@@ -5,13 +5,37 @@ import capybara.Storage;
 import capybara.TaskList;
 import capybara.Ui;
 
+/**
+ * Represents a command that searches for tasks containing a given keyword.
+ * <p>
+ * The command loads the task list from storage, filters it by the keyword,
+ * and displays the matching tasks to the user. If no matching tasks are found,
+ * a {@link CapyException} is thrown.
+ */
 public class FindCommand extends Command {
     private final String keyword;
 
+    /**
+     * Constructs a {@code FindCommand} with the specified search keyword.
+     *
+     * @param keyword the keyword to search for within task descriptions
+     */
     public FindCommand(String keyword) {
         this.keyword = keyword;
     }
 
+    /**
+     * Executes the find command.
+     * <p>
+     * Loads the task list from storage, filters it by the keyword, and displays
+     * the formatted results to the user interface. If no matching tasks are found,
+     * an exception is thrown.
+     *
+     * @param tasks    the current task list (not used directly, reloaded from storage)
+     * @param ui       the UI handler used to display results
+     * @param storage  the storage used to load tasks
+     * @throws CapyException if no tasks match the given keyword or a loading error occurs
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws CapyException {
         TaskList loaded;
